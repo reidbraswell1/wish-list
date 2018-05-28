cc6c42af8efc59b542965635528cb09b82e3ff3f
reid <reidbras@gmail.com>
2018-05-28 16:40:59 -0500

    Added Index for Ebay Items Search.

--- WishList/Controllers/EbaySearchController.cs
+++ WishList/Controllers/EbaySearchController.cs
@@ -1,93 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyCompany.Repositories;
+using MyCompany.Shared;
 
 namespace wish_list.Controllers
 {
     public class EbaySearchController : Controller
     {
-        // GET: EbaySearch
+        private readonly EbaySearchRepository _ebaySearchRepo;
+
+        public EbaySearchController(EbaySearchRepository ebaySearchRepo)
+        {
+            _ebaySearchRepo = ebaySearchRepo;
+        }
+        // GET: EbayApi
         public ActionResult Index()
         {
-            return View();
-        }
-
-        // GET: EbaySearch/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: EbaySearch/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: EbaySearch/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: EbaySearch/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: EbaySearch/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: EbaySearch/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: EbaySearch/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var items = _ebaySearchRepo.GetSearchItemResults("table");
+            return View(items);
         }
     }
 }
