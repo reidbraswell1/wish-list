02399401567d14e0cb7de57adc0e030c3274d30c
reid <reidbras@gmail.com>
2018-05-28 21:49:00 -0500

    Completing Ebay Search Repository.

--- MyCompany.Repositories/EbaySearchRepository.cs
+++ MyCompany.Repositories/EbaySearchRepository.cs
@@ -5,26 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Text;
 using Newtonsoft.Json;
 using Dapper;
+using MyCompany.Shared;
 
 //using System.Json;
 namespace MyCompany.Repositories
 {
     public class EbaySearchRepository
     {
-        private static readonly string _token;
+       // private string token;
         private readonly string _apiKey = "api-key";
         private readonly string _api = "https://api.ebay.com/buy/browse/v1/item_summary/search";
         private readonly string _apiKeyFileName = $"{Environment.CurrentDirectory}/apiKey";
         private readonly EbayRepository _ebayRepo;
 
         private readonly string _header1 = "Authorization";
-        private readonly string _header1Value = $"Bearer {_token}";
+        private string _header1Value = "Bearer ";
         private readonly string _header2 = "Content-Type";
         private readonly string _header2Value = "application/json";
         private readonly string _header3 = "X-EBAY-C-ENDUSERCTX";
@@ -35,10 +33,12 @@ namespace MyCompany.Repositories
         }
         public IEnumerable<ItemSummary> GetSearchItemResults(string item)
         {
-            var _token = GetApiToken(_apiKey);
+            var token = GetApiToken(_apiKey);
+            this._header1Value = _header1Value + token;
             var options = $"?q={item}&limit=15";
-            HttpGet(_api + options);
-            return new List<ItemSummary>();
+            var webResponse = HttpGet(_api + options);
+            return ParseWebResponse(webResponse);
+            //return new List<ItemSummary>();
         }
         private string GetApiToken(string key)
         {
@@ -67,10 +67,23 @@ namespace MyCompany.Repositories
                 throw e;
             }
         }
+        private IEnumerable<ItemSummary> ParseWebResponse(WebResponse response)
+        {
+            try
+            {
+                Stream dataStream = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                var json = reader.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<RootObject>(json);
+                response.Dispose();
+                return items.itemSummaries;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
-
-
-
-
-
 }
